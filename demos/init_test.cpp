88b020d9bd5fbce578ff5f55e44700c44bbc437a
k00ndra <ondra.kodejs@gmail.com>
2023-12-01 17:21:55 +0100

    added tools

---
+++ demos/init_test.cpp
@@ -0,0 +1,16 @@
+//
+// Created by koondra on 1.12.23.
+//
+
+#include <sys/socket.h>
+#include <iostream>
+
+
+using namespace std;
+
+int main(){
+    cout<<"You stuupid\n"<<endl;
+    return 0;
+}
+
+
