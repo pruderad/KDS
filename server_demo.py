5290749482d6e47e1748ef90c1c57e533cc21b1c
k00ndra <ondra.kodejs@gmail.com>
2023-12-07 00:18:56 +0100

    debugging backup

--- server_demo.py
+++ server_demo.py
@@ -3,11 +3,11 @@ import time
 import crcmod
 
 SERVER_IP_ADRESS = ''
-FILE_PATH = './data/ajm.txt'
+FILE_PATH = './data/n1_driver.jpg'
 
 if __name__ == '__main__':
 
-    server = Server(ack_timout_s=0.3, sender_freq_hz=1, reciever_freq_hz=1)
+    server = Server(ack_timout_s=0.1, sender_freq_hz=100, reciever_freq_hz=100)
     server.serve_file(file_path=FILE_PATH, window_size=10)
 
     print('done')
